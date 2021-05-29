9dd6897bc63d7a8326ec6707362258d2cc51648a
medram <mramouchy@gmail.com>
2021-05-29 02:02:56 +0100

    added settings: incoming & outgoing calls options!

---
+++ screens/SettingsScreen.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { Switch } from 'react-native-elements'
+import { useSelector, useDispatch } from 'react-redux'
+
+import { TOGGLE_INCOMING_CALLS, TOGGLE_OUTGOING_CALLS } from '../store/actions/auth'
+import * as Config from '../Config'
+
+
+export function SettingsScreen(props)
+{
+    const { incomingCalls, outgoingCalls } = useSelector(state => state.main.settings)
+    const dispatch = useDispatch()
+
+    const changeIncomingCallsHandler = () => {
+        dispatch({ type: TOGGLE_INCOMING_CALLS })
+    }
+
+    const changeOutgoingCallsHandler = () => {
+        dispatch({ type: TOGGLE_OUTGOING_CALLS })
+    }
+
+    return <View style={styles.container}>
+        <SettingItem title='Detect Incoming Calls' desc='Send SMS messages to incoming calls only.'
+            value={incomingCalls} onChange={changeIncomingCallsHandler} />
+        <SettingItem title='Detect Outgoing Calls' desc='Send SMS messages to outgming calls only.'
+            value={outgoingCalls} onChange={changeOutgoingCallsHandler} />
+    </View>
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    }
+})
+
+// Setting React Item
+
+const SettingItem = (props) => {
+
+    return <View style={itemStyles.container}>
+        <View>
+            <Text style={itemStyles.title}>{props.title}</Text>
+            <Text style={itemStyles.desc}>{props.desc}</Text>
+        </View>
+        <View style={itemStyles.rightbox}>
+            <Switch value={props.value} onChange={props.onChange} color={Config.COLORS.primary} />
+        </View>
+    </View>
+}
+
+const itemStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        //backgroundColor: '#CCC',
+        paddingVertical: 5,
+        paddingHorizontal: 20,
+        borderColor: '#ddd',
+        borderBottomWidth: 1,
+        maxHeight: 75,
+    },
+    rightbox: {
+        flex: 1,
+    },
+    title: {
+        fontSize: 18,
+    },
+    desc: {
+        fontSize: 13,
+    },
+})
--- store/reducers/mainReducer.js
+++ store/reducers/mainReducer.js
@@ -1,10 +1,14 @@
-import { SAVE_CONTACTS, TOGGLE_BUTTON_STATUS, FIRST_START } from "../actions/auth"
+import { SAVE_CONTACTS, TOGGLE_BUTTON_STATUS, FIRST_START, TOGGLE_INCOMING_CALLS, TOGGLE_OUTGOING_CALLS } from "../actions/auth"
 
 
 const initialState = {
     firstStart: true,
     buttonStatus: false,
     contactList: [],
+    settings: {
+        incomingCalls: true,
+        outgoingCalls: true
+    },
 }
 
 const mainReducer = (state = initialState, action) => {
@@ -12,12 +16,30 @@ const mainReducer = (state = initialState, action) => {
     switch(action.type)
     {
         case TOGGLE_BUTTON_STATUS:
+        {
             let value = !!action.payload ? action.payload : !state.buttonStatus
             return { ...state, buttonStatus: value }
+        }
+
         case SAVE_CONTACTS:
             return { ...state, contactList: action.payload }
+
         case FIRST_START:
             return { ...state, firstStart: action.payload }
+
+        case TOGGLE_INCOMING_CALLS:
+        {
+
+            let value = !!action.payload ? action.payload : !state.settings.incomingCalls
+            return { ...state, settings: { ...state.settings, incomingCalls: value } }
+        }
+
+        case TOGGLE_OUTGOING_CALLS:
+        {
+            let value = !!action.payload ? action.payload : !state.settings.outgoingCalls
+            return { ...state, settings: { ...state.settings, outgoingCalls: value } }
+        }
+
         default:
             return state
     }
